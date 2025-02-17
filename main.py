29bec8b32db4904790b2927262c10d38dd06f5fc
itmightbemiles <miles.tugman@gmail.com>
2025-02-17 09:46:03 -0600

    B: more fumbling, now with rudimentary elements!

--- main.py
+++ main.py
@@ -2,7 +2,22 @@
 # the open-source pygame library
 # throughout this file
 import pygame
+import sys
 from constants import *
+from player import Player
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
+
+
+updatable = pygame.sprite.Group()
+drawable = pygame.sprite.Group()
+asteroids = pygame.sprite.Group()
+
+AsteroidField.containers = (updatable,)
+Player.containers = (updatable, drawable)
+Asteroid.containers = (asteroids, updatable, drawable)
+player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+asteroid_field = AsteroidField()
 
 def main():
     pygame.init()
@@ -10,6 +25,8 @@ def main():
     print("Starting asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
+    clock = pygame.time.Clock()
+    dt = 0
 
     while True:
         for event in pygame.event.get():
@@ -17,7 +34,21 @@ def main():
                 return
         #draw frame
         screen.fill("black")
+        dt= clock.tick(60) /1000
+        updatable.update(dt)
+
+        for sprite in asteroids:
+            if player.collision(sprite):
+                print("Game over!")
+                sys.exit()
+
+        for sprite in drawable:
+            sprite.draw(screen)
+
+
         pygame.display.flip()
+    
+
 
 if __name__ == "__main__":
     main()
